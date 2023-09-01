07c845064772ae179d9d41f66a6ced8ad9b4f322
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2023-08-31 21:05:09 -0400

    Added tree_node class

--- tree_node.py
+++ tree_node.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 class TreeNode:
     def __init__(self, value):
         self.value = value
@@ -17,6 +19,22 @@ class TreeNode:
             current_node = nodes_to_visit.pop()
             print(current_node.value)
             nodes_to_visit.extend(current_node.children)
+
+    def __str__(self):
+        stack = deque()
+        stack.append([self, 0])
+        level_str = "\n"
+        while len(stack) > 0:
+            node, level = stack.pop()
+
+            if level > 0:
+                level_str += "| "*(level - 1) + "|-"
+            level_str += str(node.value)
+            level_str += "\n"
+            level += 1
+            for child in reversed(node.children):
+                stack.append([child, level])
+        return level_str
 
 
 root = TreeNode("CEO")
