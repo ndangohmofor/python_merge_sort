0d5546c3d5623b41416dc47137cd3e87218e63e2
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2023-09-08 23:45:21 -0400

    changed the helper methods into static methods

--- max_heap.py
+++ max_heap.py
@@ -1,28 +1,31 @@
+def parent_idx(idx):
+    return idx // 2
+
+
+def left_child_idx(idx):
+    return idx * 2
+
+
+def right_child_idx(idx):
+    return (idx * 2) + 1
+
+
 class MaxHeap:
     def __init__(self):
         self.heap_list = [None]
         self.count = 0
 
-    def parent_idx(self, idx):
-        return idx // 2
-
-    def left_child_idx(self, idx):
-        return idx * 2
-
-    def right_child_idx(self, idx):
-        return (idx * 2) + 1
-
     def heapify_up(self):
         print("Heapifying up")
         idx = self.count
-        while self.parent_idx(idx) > 0:
+        while parent_idx(idx) > 0:
             element = self.heap_list[idx]
-            parent = self.heap_list[self.parent_idx(idx)]
+            parent = self.heap_list[parent_idx(idx)]
             if parent < element:
                 print("Swapping {0} with {1}".format(parent, element))
-                self.heap_list[self.parent_idx(idx)] = element
+                self.heap_list[parent_idx(idx)] = element
                 self.heap_list[idx] = parent
-            idx = self.parent_idx(idx)
+            idx = parent_idx(idx)
         print("Heap Restored {0}".format(self.heap_list))
 
     def add(self, element):
