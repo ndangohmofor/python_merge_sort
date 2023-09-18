a24a1ed252c080d5689d547aa9e49d6734f7df3c
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2023-09-17 21:40:17 -0400

    Starting to implement the heapsort

---
+++ heapsort.py
@@ -0,0 +1,13 @@
+from max_heap import MaxHeap
+
+
+def heapsort(lst):
+    sort = []
+    max_heap = MaxHeap()
+    for el in lst:
+        max_heap.add(el)
+    print(max_heap.heap_list)
+
+
+my_list = [99, 22, 61, 10, 21, 13, 23, 44, 200, 54]
+sorted_list = heapsort(my_list)
