0c4a2e46dc46a43f3dae7da3c26c868c4cb35243
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2023-09-21 23:35:51 -0400

    Added the recursive step to the depth first search

--- graphy_dfs.py
+++ graphy_dfs.py
@@ -5,5 +5,11 @@ def dfs(graph, current_vertex, target_value, visited=None):
     if current_vertex == target_value:
         return visited
 
+    for neighbor in graph[current_vertex]:
+        if neighbor not in visited:
+            path = dfs(graph, neighbor, target_value, visited)
+            if path:
+                return path
+
 
 print(dfs(None, "bees?", None))
