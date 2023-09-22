942a180bd439dd2532d87cf9f6ef17ae3d2150de
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2023-09-21 23:26:02 -0400

    Added the base case for the dfs recursive function

--- graphy_dfs.py
+++ graphy_dfs.py
@@ -2,7 +2,8 @@ def dfs(graph, current_vertex, target_value, visited=None):
     if visited is None:
         visited = []
     visited.append(current_vertex)
-    return visited
+    if current_vertex == target_value:
+        return visited
 
 
 print(dfs(None, "bees?", None))
