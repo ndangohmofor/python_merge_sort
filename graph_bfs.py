e9bee770747f2a932ceb1f28b2132df9bf87163a
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2023-09-22 08:02:48 -0400

    checking every vertex in the queue and looking at their adjacent vertices

--- graph_bfs.py
+++ graph_bfs.py
@@ -4,4 +4,11 @@ def bfs(graph, start_vertex, target_value):
 
     bfs_queue = [vertex_and_path]
 
-    visited = {}
+    visited = set()
+
+    while bfs_queue:
+        current_vertex, path = bfs_queue.pop(0)
+        visited.add(current_vertex)
+
+        for neighbor in graph[current_vertex]:
+            pass
