0001eef615c18ce7a58591a9f4f618dd3646ca55
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2023-10-09 06:48:33 -0400

    add print_graph method in main script file

---
+++ traveling_salesperson.py
@@ -0,0 +1,15 @@
+import random
+from random import randrange
+from ts_graph import Graph
+from ts_vertex import Vertex
+
+
+def print_graph(graph):
+    for vertex in graph.graph_dict:
+        print("")
+        print(vertex + " connected to")
+        vertex_neighbors = graph.graph_dict[vertex].edges
+        if len(vertex_neighbors) == 0:
+            print("No edges!")
+        for adjacent_vertex in vertex_neighbors:
+            print("=> " + adjacent_vertex)
