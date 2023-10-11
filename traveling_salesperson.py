8df7d0db7b796141e2c063332beca8f3a34d004d
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2023-10-11 07:03:29 -0400

    added the traveling_salesperson method which holds the algorithm

--- traveling_salesperson.py
+++ traveling_salesperson.py
@@ -42,3 +42,39 @@ def visited_all_nodes(visited_vertices):
         if visited_vertices[vertex] == "unvisited":
             return False
     return True
+
+
+def traveling_salesperson(graph):
+    final_path = ""
+    visited_vertices = {x: "unvisited" for x in graph.graph_dict}
+    current_vertex = random.choice(list(graph.graph_dict))
+    visited_vertices[current_vertex] = "visited"
+    final_path += current_vertex
+
+    done = visited_all_nodes(visited_vertices)
+
+    while not done:
+        current_vertex_edges = graph.graph_dict[current_vertex].get_edges()
+        current_vertex_edge_weights = {}
+        for edge in current_vertex_edges:
+            current_vertex_edge_weights[edge] = graph.graph_dict[current_vertex].get_edge_weight(edge)
+        found_next_vertex = False
+        next_vertex = ""
+        while not found_next_vertex:
+            if not current_vertex_edge_weights:
+                break
+            next_vertex = min(current_vertex_edge_weights, key=current_vertex_edge_weights.get)
+            if visited_vertices[next_vertex] == "visited":
+                current_vertex_edge_weights.pop(next_vertex)
+            else:
+                found_next_vertex = True
+        if not current_vertex_edge_weights:
+            done = True
+        else:
+            current_vertex = next_vertex
+            visited_vertices[current_vertex] = "visited"
+            final_path += current_vertex
+        done = visited_all_nodes(visited_vertices)
+    print(final_path)
+
+
