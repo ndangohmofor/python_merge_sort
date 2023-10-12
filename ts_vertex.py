96189da455a1c07171a3d104a42d7058775b6653
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2023-10-12 05:07:42 -0400

    Fixed the call to self.edges.keys() and edges is not a function

--- ts_vertex.py
+++ ts_vertex.py
@@ -7,7 +7,7 @@ class Vertex:
         self.edges[vertex] = weight
 
     def get_edges(self):
-        return list(self.edges().keys())
+        return list(self.edges.keys())
 
     def get_edge_weight(self, edge):
         return self.edges[edge]
