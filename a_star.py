a22c4bdd5c12f8617f9b131320f38d6a506367e1
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2023-10-13 08:25:20 -0400

    Updated the Dijkstra's algo to produce a* algorithm

--- a_star.py
+++ a_star.py
@@ -3,11 +3,11 @@ from math import inf
 from vertex import Vertex
 
 
-def a_star(graph, start: Vertex):
-    distances = {}
+def a_star(graph, start, end):
+    paths_and_distances = {}
     for vertex in graph:
-        distances[vertex] = inf
-    distances[start] = 0
+        paths_and_distances[vertex] = [inf, [start.name]]
+    paths_and_distances[start][0] = 0
 
     vertices_to_explore = [(0, start)]
 
@@ -15,7 +15,9 @@ def a_star(graph, start: Vertex):
         curr_dist, curr_vert = heapq.heappop(vertices_to_explore)
         for neighbor, edge_weight in graph[curr_vert]:
             new_dist = curr_dist + edge_weight
-            if new_dist < distances[neighbor]:
-                distances[neighbor] = new_dist
+            new_path = paths_and_distances[curr_vert][1] + [neighbor.name]
+            if new_dist < paths_and_distances[neighbor][0]:
+                paths_and_distances[neighbor][0] = new_dist
+                paths_and_distances[neighbor][1] = new_path
                 heapq.heappush(vertices_to_explore, neighbor)
-    return distances
+    return paths_and_distances[end][1]
